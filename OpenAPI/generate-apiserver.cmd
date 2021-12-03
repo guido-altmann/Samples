1855ef6caf208d04911681206fed017009b49949
Guido Altmann <guido.altmann@laytec.de>
2021-12-03 09:19:52 +0100

    remove Open API Demo

--- OpenAPI/generate-apiserver.cmd
+++
@@ -1,5 +0,0 @@
-REM Execute with a Node.js command prompt.
-REM Further infos at: https://openapi-generator.tech/
-REM
-
-openapi-generator generate -i openapi.json -g aspnetcore --additional-properties packageName=OpenApiServer,aspnetCoreVersion=2.1 -o ./Server
--- OpenAPI/generate-client.cmd
+++
@@ -1,5 +0,0 @@
-REM Execute with a Node.js command prompt.
-REM Further infos at: https://openapi-generator.tech/
-REM
-
-openapi-generator generate -i openapi.json -g csharp-netcore --additional-properties packageName=OpenApiClient -o ./Client
